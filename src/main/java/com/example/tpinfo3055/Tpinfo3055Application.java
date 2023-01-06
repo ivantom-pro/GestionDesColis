14c0e57b9b9e00e464864703b20b1f1e744f1a3f
ivantom-pro <ivantom.python@gmail.com>
2023-01-06 21:50:16 +0100

    second commit

---
+++ src/main/java/com/example/tpinfo3055/Tpinfo3055Application.java
@@ -0,0 +1,13 @@
+package com.example.tpinfo3055;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpinfo3055Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpinfo3055Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tpinfo3055/Tpinfo3055ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpinfo3055;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpinfo3055ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
